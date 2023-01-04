2d76d38fa1542828ba9ad86fab892d4e7544b801
Amir-UL <mangoleo51@gmail.com>
2023-01-04 17:32:26 +0600

    a servlet from another

--- demo-servlet/LEARND.md
+++ demo-servlet/LEARND.md
@@ -1,1 +1,2 @@
 #### The moment I ```extend``` a normal Class with ```HttpServlet``` that class became Servlet Class.
+#### If You want to work with only POST Request You should use ```doPost()``` method Instead of ```service()``` method.
--- demo-servlet/src/main/java/com/example/demoservlet/MathO.java
+++ demo-servlet/src/main/java/com/example/demoservlet/MathO.java
@@ -4,10 +4,19 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class MathO extends HttpServlet {
     // Have to Use the Service Method in order to add any Service LIKE here adding Numbers
     // Have to User HttpServletRequest and HttpServletResponse to get the Request and Response from the Client
-    public void service(HttpServletRequest request, HttpServletResponse response){
+//    This Line of Code is also Valid for both GET and POST
+//    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Only for Post Request
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // catching the Request from the Client
+        // Getting the Query String from the Request
         int i = Integer.parseInt(request.getParameter("N1"));
         int j = Integer.parseInt(request.getParameter("N2"));
 
@@ -15,5 +24,9 @@ public class MathO extends HttpServlet {
         System.out.println(k);
 
         // You have to set the Flow in web.xml file
+
+        // Printing data Server to Client
+        PrintWriter out = response.getWriter();
+        out.println("Result is " + k);
     }
 }
