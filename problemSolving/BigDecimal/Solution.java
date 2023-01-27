803fdd40e9c0735491d0e89f882b6c33d2b438eb
Amir-UL <mangoleo51@gmail.com>
2023-01-27 18:52:25 +0600

    java based problems

---
+++ problemSolving/BigDecimal/Solution.java
@@ -0,0 +1,20 @@
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<BigDecimal> s = new ArrayList<>();
+        for (int i = 0; i < n; i ++ ){
+        BigDecimal totalQuantity = new BigDecimal(sc.next());
+            s.add(totalQuantity);
+        }
+        sc.close();
+        s.sort((a, b) -> b.compareTo(a));
+        for (BigDecimal i : s) {
+            System.out.println(i);
+        }
+
+    }
+}
